fe042a5c75b8ffd8a7aafc608da93299b9c3363a
Chris Toomey <chris@ctoomey.com>
2018-03-17 12:47:17 -0400

    Add react-native-dotenv for token loading

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import {GITHUB_TOKEN} from 'react-native-dotenv';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>token: {GITHUB_TOKEN}</Text>
+        <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
   }
