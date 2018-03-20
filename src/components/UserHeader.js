8c19d720aac9c555300e380748559a3b32b9e35c
Chris Toomey <chris@ctoomey.com>
2018-03-19 22:14:31 -0400

    Fix header spacing

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -5,9 +5,11 @@ import gql from 'graphql-tag';
 const UserHeader = ({user}) => (
   <View style={styles.row}>
     <Image source={{uri: user.avatarUrl}} style={styles.avatar} />
-    <Text>{user.login}</Text>
-    <Text>{user.name || '(name not provided)'}</Text>
-    <Text>{user.bio}</Text>
+    <View style={{flex: 1}}>
+      <Text style={styles.loginTitle}>{user.login}</Text>
+      <Text style={styles.name}>{user.name || '(name not provided)'}</Text>
+      <Text>{user.bio}</Text>
+    </View>
   </View>
 );
 
@@ -26,9 +28,17 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     marginBottom: 10,
   },
+  loginTitle: {
+    fontSize: 24,
+  },
+  name: {
+    fontSize: 14,
+    color: '#666',
+    marginBottom: 5,
+  },
   avatar: {
-    width: 75,
-    height: 75,
+    width: 100,
+    height: 100,
     borderRadius: 3,
     marginRight: 10,
   },
