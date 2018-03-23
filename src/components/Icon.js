fb58bcfe57b2594b81b2131e117ee0be1e26ba66
Chris Toomey <chris@ctoomey.com>
2018-03-23 11:24:40 -0400

    Add styled-components and convert Icon

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,13 +1,8 @@
-import React from 'react';
-import {Image, StyleSheet} from 'react-native';
+import styled from 'styled-components';
 
-const Icon = ({source}) => <Image source={source} style={styles.icon} />;
-
-const styles = StyleSheet.create({
-  icon: {
-    width: 16,
-    height: 16,
-  },
-});
+const Icon = styled.Image`
+  width: 16px;
+  height: 16px;
+`;
 
 export default Icon;
