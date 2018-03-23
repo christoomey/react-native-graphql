0bdfb182d43242f0a236d082ec1f3e9ca8e825fb
Chris Toomey <chris@ctoomey.com>
2018-03-23 13:06:26 -0400

    Fix up Language

--- src/components/Language.js
+++ src/components/Language.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import styled from 'styled-components';
 import gql from 'graphql-tag';
+import Row from './Row';
 
 const Language = ({language, style}) =>
   language && (
-    <View style={[{flexDirection: 'row'}, style]}>
+    <Row style={style}>
       <Circle color={language.color} />
-      <Text style={{marginLeft: 2}}>{language.name}</Text>
-    </View>
+      <SpacedText>{language.name}</SpacedText>
+    </Row>
   );
 
-const Circle = ({color}) => (
-  <View
-    collapsable={false}
-    style={{
-      backgroundColor: color,
-      borderRadius: 6,
-      height: 12,
-      width: 12,
-    }}
-  />
-);
+const Circle = styled.View`
+  background-color: ${props => props.color};
+  border-radius: 6px;
+  height: 12px;
+  width: 12px;
+`;
+
+const SpacedText = styled.Text`
+  margin-left: 2px;
+`;
 
 export const LANGUAGE_FRAGMENT = gql`
   fragment Language on Language {
