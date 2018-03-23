d0c74473447dc2d9db7d7cf9b997bde7b6314ef4
Chris Toomey <chris@ctoomey.com>
2018-03-22 21:10:24 -0400

    Transition to apollo-boost

--- App.js
+++ App.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import {ApolloClient} from 'apollo-client';
-import {HttpLink} from 'apollo-link-http';
-import {setContext} from 'apollo-link-context';
-import {InMemoryCache} from 'apollo-cache-inmemory';
+import ApolloClient from 'apollo-boost';
 import {ApolloProvider} from 'react-apollo';
 import {GITHUB_TOKEN} from 'react-native-dotenv';
 import {NativeRouter} from 'react-router-native';
@@ -10,18 +7,15 @@ import {Navigation, Card} from 'react-router-navigation';
 import SearchPage from './src/pages/SearchPage';
 import UserPage from './src/pages/UserPage';
 
-const authLink = setContext((_, {headers}) => ({
-  headers: {
-    ...headers,
-    authorization: `Bearer ${GITHUB_TOKEN}`,
-  },
-}));
-
-const httpLink = new HttpLink({uri: 'https://api.github.com/graphql'});
-
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
+  uri: 'https://api.github.com/graphql',
+  request: async operation => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${GITHUB_TOKEN}`,
+      },
+    });
+  },
 });
 
 const App = () => (
